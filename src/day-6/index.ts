511b084ed91a2917e92d7c743f9e744f5da11f8f
Justin Bird <justin@thewordisbird.dev>
2023-12-05 22:21:36 -0800

    cleanup day-6

--- src/day-6/index.ts
+++ src/day-6/index.ts
@@ -1,18 +1,15 @@
-const testData = [[7,9], [15,40], [30, 200]]
-const partOneData = [[51,377], [69, 1171], [98, 1224], [78, 1505]]
-const partTwoData = [[51699878,377117112241505]]
+const testData = [[7, 9], [15, 40], [30, 200]]
+const partOneData = [[51, 377], [69, 1171], [98, 1224], [78, 1505]]
+const partTwoData = [[51699878, 377117112241505]]
 
 function solution(races: number[][]) {
-
-  let product = 1
-  for (let race of races) {
-    const [T, R] = race;
+  return races.reduce((acc, cur) => {
+    const [T, R] = cur;
     const x1 = T/2 + Math.sqrt((T*T) - (4*R))/-2;
     const x2 = T/2 - Math.sqrt((T*T) - (4*R))/-2;
 
-    product = product * ((Math.ceil(x2) - Math.floor(x1))-1);
-  }
-  return product
+    return acc * ((Math.ceil(x2) - Math.floor(x1))-1);
+    }, 1);
 }
 
 const testSolution = solution(testData);
